1fa5e5aa1a6a999935369ca889c95e6708ab5c74
dilshan0786 <139648348+dilshan0786@users.noreply.github.com>
2024-09-03 14:11:49 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
             languages: ['Hindi-Multi Audio'],
             type: 'Action',
             poster: 'Kalki2898AD.jpg',
-            screenshots: ['Kalki2898AD_SS1.png', 'Kalki2898AD_SS2.png', 'Kalki2898AD_SS3.png', 'Kalki2898AD_SS4.png', 'Kalki2898AD_SS5.png', 'Kalki2898AD_SS6.png'],
+            screenshots: ['Kalki2898AD_SS1.jpg', 'Kalki2898AD_SS2.jpg', 'Kalki2898AD_SS3.jpg', 'Kalki2898AD_SS4.jpg', 'Kalki2898AD_SS5.jpg', 'Kalki2898AD_SS6.png'],
             downloadLinks: {
                 '480p [600 MB]': 'http://tnlinks.in/AYN8u',
                 '720p [1.7 GB]': 'http://tnlinks.in/1VJil',
